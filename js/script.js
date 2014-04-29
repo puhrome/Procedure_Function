e3185b6110edee601daa8e7e7d7ca89cae1d00d9
puhrome <phirom.yim@fullsail.edu>
2014-04-29 18:49:44 -0400

    Procedure_Function
    Explaining what a procedure is and how it is different from a Function

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/**
+ * Procedure Created by Phirom Yim on 4/29/14.
+ */
+//This is a procedure
+function calcAreaP(width, height){
+    var area = width * height;
+    console.log(area);//no return
+//Procedure is the process of a function
+//explains the steps that are in a function
+//does not create any kind of return
